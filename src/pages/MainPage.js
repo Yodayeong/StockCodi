f02880c00f11e9fa099c49b0482bbab498011394
kimmokalover <kimmokalover@gmail.com>
2023-05-13 22:35:43 +0900

    design : MainPage 디자인 추가 [김승준]

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,79 +1,99 @@
 import '../styles/MainPage.css';
 import phone from '../img/phone.png';
 import Group18403 from '../img/Group18403.png'
+import React, { useEffect, useState } from 'react';
 
-function Stocodi(){
-    return (
-        <div className = "Stocodi">
-            Stocodi 
-        </div>
-    )
-}
-
-function InvestmentExperiment(){
-    return (
-        <div className = "InvestmentExperiment">
-            투자실험 
-        </div>
-    )
-}
-function Study(){
-    return (
-        <div className = "Study">
-            스터디 
-        </div>
-    )
-}
-function Community(){
-    return (
-        <div className = "Community">
-            커뮤니티 
-        </div>
-    )
-}
-
-function Login(){
-    return (
-        <div className = "Login">
-            로그인 
-        </div>
-    )
-}
-function Signup(){
-    return (
-        <div className = "Signup">
-            회원가입 
-        </div>
-    )
-}
 function MainPageHead(){
     return(
         <div className = "header-container">
-            <Stocodi />
-            <InvestmentExperiment />
-            <Study />
-            <Community />
-            <Login />
-            <Signup />
+            <div className = "Stocodi">
+                Stocodi 
+            </div>
+            <div className = "InvestmentExperiment">
+                투자실험 
+            </div>
+            <div className = "Study">
+                스터디 
+            </div>
+            <div className = "Community">
+                커뮤니티 
+            </div>
+            <div className = "Login">
+                로그인 
+            </div>
+            <div className = "Signup">
+                회원가입 
+            </div>
         </div>
     )
 }
-function MainPageBody(){
-    return(
+
+function ImageTable(){
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        // 서버에서 이미지 url을 받아온다는 가정
+        const imageUrls = [
+          'https://example.com/image1.jpg',
+          'https://example.com/image2.jpg',
+          'https://example.com/image3.jpg',
+          'https://example.com/image4.jpg',
+        ];
+        setImages(imageUrls);
+      }, []);
+    
+      return (
+        <table>
+          <tbody>
+            <tr>
+              {images.slice(0, 4).map((url, index) => (
+                <td key={index}>
+                  <img src={url} alt={`Image ${index}`} width="200" height="200" />
+                </td>
+              ))}
+            </tr>
+          </tbody>
+        </table>
+      );
+}
+
+
+function MainPageBody() {
+    return (
+      <div>
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <div className="main-text">
+            투자가 어려우신가요?
+            <br />
+            함께 연습해보면 어떨까요!
+          </div>
+          <div className='img1'>
+            <img
+              src={phone}
+              alt="phone"
+              style={{ width: "50%", height: "auto", float: "right", marginLeft: "10px"}}
+            />
+          </div>
+        </div>
+        <div className = "img2">
+          <img
+            src={Group18403}
+            alt="group"
+            style={{ verticalAlign: "bottom", marginRight: "100px"}}
+          />
+        </div>
         <div>
-            <div style = {{width: '30%'}}>
-                <div>
-                    <img src={phone} alt="phone" />
-                </div>
-            </div>
-            <div>
-                <div style={{width: '50%'}}>
-                    <img src={Group18403} alt="group" />
-                </div>
-            </div>
+            교육 컨텐츠
         </div>
-    )
-}
+
+        <div>
+            <ImageTable />
+        </div>
+      </div>
+    );
+  }
+  
+  
 function MainPageContainer(){
     return(
         <div>
