44de8660149cd01cea23e572d9554165e0f3d123
kimmokalover <kimmokalover@gmail.com>
2023-05-18 11:51:13 +0900

    feat : 로그인 회원가입 토글 기능 추가 및 카태고리 토글 기능 추가 [김승준]

--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -2,30 +2,88 @@ import '../styles/MainPage.css';
 import phone from '../img/phone.png';
 import Group18403 from '../img/Group18403.png'
 import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-function MainPageHead(){
-    return(
-        <div className = "header-container">
-            <div className = "Stocodi">
-                Stocodi 
-            </div>
-            <div className = "InvestmentExperiment">
-                투자실험 
-            </div>
-            <div className = "Study">
-                스터디 
-            </div>
-            <div className = "Community">
-                커뮤니티 
-            </div>
-            <div className = "Login">
-                로그인 
-            </div>
-            <div className = "Signup">
-                회원가입 
-            </div>
-        </div>
-    )
+
+
+
+const StyledButton = styled.button`
+  background-color: ${props => (props.clicked ? '#00A969' : 'white')};
+  color : ${props => (props.clicked ? 'white' : '#7D8790')};
+  border: none;
+  font-size : 14px;
+  font-weight : bold;
+  border-radius: 20px;
+  padding: 10px 20px;
+  cursor: pointer;
+`;
+
+const CatagoryComponent = () =>{
+  const [selectedCatagory, setSelectedCatagory] = useState(1);
+
+  const handleCategoryChange = (category) =>{
+    setSelectedCatagory(category);
+  };
+
+  return (<div className='Subheader-container'>
+      <div className={selectedCatagory === 1 ? "InvestmentExperimentSelected" : "InvestmentExperiment"} onClick={() => handleCategoryChange(1)}>투자실험</div>
+      <div className={selectedCatagory === 2 ? "StudySelected" : "Study"} onClick={() => handleCategoryChange(2)}>스터디</div>
+      <div className={selectedCatagory === 3 ? "CommunitySelected" : "Community"} onClick={() => handleCategoryChange(3)}>커뮤니티</div>
+  </div>)
+}
+
+
+
+
+const Button = ({clickState, name, onClick}) => {
+  const [clicked, setClicked] = useState(clickState);
+  const handleClick = () => {
+      setClicked(!clicked);
+      onClick()
+  };
+
+  return (
+    <StyledButton clicked={clicked} onClick={handleClick}>
+      <div className='Logsi'>{name}</div>
+    </StyledButton>
+  );
+};
+
+function MainPageHead() {
+  const [loginClicked, setLoginClicked] = useState(false);
+  const [signupClicked, setSignupClicked] = useState(true);
+
+  const handleLoginClick = () => {
+      if(!loginClicked)
+      {
+        setLoginClicked(!loginClicked);
+        setSignupClicked(!signupClicked);
+      }
+  };
+
+  const handleSignupClick = () => {
+    if (!signupClicked){
+    setLoginClicked(!loginClicked);
+    setSignupClicked(!signupClicked);
+    }
+  };
+
+  return (
+    <div className="header-container">
+      <div className="Stocodi">Stocodi</div>
+      <CatagoryComponent />
+      <div className="Login">
+        <StyledButton clicked={loginClicked} onClick={handleLoginClick}>
+          로그인
+        </StyledButton>
+      </div>
+      <div className="Signup">
+        <StyledButton clicked={signupClicked} onClick={handleSignupClick}>
+          회원가입
+        </StyledButton>
+      </div>
+    </div>
+  );
 }
 
 function ImageTable(){
@@ -82,7 +140,7 @@ function MainPageBody() {
             style={{ verticalAlign: "bottom", marginRight: "100px"}}
           />
         </div>
-        <div>
+        <div className='contents-text'>
             교육 컨텐츠
         </div>
 
