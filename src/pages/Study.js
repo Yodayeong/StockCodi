7aac5f5baa052305bbcf9ed0cfc951479e8601a8
Yodayeong <duekdud01@naver.com>
2023-05-27 00:24:38 +0900

    feat: 스터디 페이지 검색창/토글 [여다영]

--- src/pages/Study.js
+++ src/pages/Study.js
@@ -1,61 +1,136 @@
 import { useNavigate } from "react-router-dom";
 import '../styles/Study.css';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
+import { FiSearch } from 'react-icons/fi';
 
-const CatagoryComponent = () =>{
+// Nav Bar
+const CatagoryComponent = () => {
   return (
-  <div className='Subheader-container'>    
-    <div className="nav-items">
-      투자실혐
+    <div className='Subheader-container'>    
+      <div className="nav-items">
+        투자실혐
+      </div>
+      <div className="nav-items">
+        스터디
+      </div>
+      <div className="nav-items">
+        커뮤니티
+      </div>
     </div>
-    <div className="nav-items">
-      스터디
-    </div>
-    <div className="nav-items">
-      커뮤니티
-    </div>
-  </div>)
+  );
 }
 
-function MainPageHead() {
+function StudyPageHead() {
   // link
   const moveLogin = useNavigate();
   const moveSignup = useNavigate();
 
-  function goLogin(){
-      moveLogin('/login');
+  function goLogin() {
+    moveLogin('/login');
   }
-  function goSignup(){
-      moveSignup('/signup');
+  
+  function goSignup() {
+    moveSignup('/signup');
   }
 
   return (
     <div className="header-container">
       <div className="Stocodi">Stocodi</div>
       <CatagoryComponent />
-        <div className="account-items" onClick={goLogin}>
-          로그인
-        </div>
-        <div className="account-items" onClick={goSignup}>
-          회원가입
-        </div>
+      <div className="account-items" onClick={goLogin}>
+        로그인
+      </div>
+      <div className="account-items" onClick={goSignup}>
+        회원가입
+      </div>
     </div>
   );
 }
 
-function MainPageBody() {
-    return (
-      <div></div>
-    );
+// Search
+function Search() {
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearch = () => {
+    // 검색 기능을 수행하는 로직을 작성하세요
+    console.log('Search:', searchQuery);
+  };
+
+  return (
+    <div className="search-bar">
+      <div className="search-input-container">
+        <input type="text" placeholder="콘텐츠 검색" value={searchQuery} onChange={handleInputChange} />
+        <button onClick={handleSearch}>
+          <FiSearch />
+        </button>
+      </div>
+    </div>
+  );
 }
 
-function StudyContainer(){
-    return(
+// Toggle
+function Toggle() {
+    const [contents, setContents] = useState([
+        { id: 1, title: '컨텐츠 1', likes: 10 },
+        { id: 2, title: '컨텐츠 2', likes: 5 },
+        { id: 3, title: '컨텐츠 3', likes: 8 },
+      ]);
+      const [isLatest, setIsLatest] = useState(true);
+    
+      const sortContents = () => {
+        const sortedContents = [...contents];
+        if (isLatest) {
+          sortedContents.sort((a, b) => b.id - a.id); // 최신순 정렬
+        } else {
+          sortedContents.sort((a, b) => b.likes - a.likes); // 좋아요순 정렬
+        }
+        setContents(sortedContents);
+      };
+    
+      const toggleSort = () => {
+        setIsLatest((prevState) => !prevState);
+        sortContents(); // 정렬 함수 호출
+      };
+    
+      return (
         <div>
-            <MainPageHead />
-            <MainPageBody />
+          <div className="sort-toggle">
+            <button onClick={toggleSort}>
+              {isLatest ? '최신순' : '좋아요순'}
+            </button>
+          </div>
+          <div className="content-list">
+            {contents.map((content) => (
+              <div key={content.id}>
+                <h3>{content.title}</h3>
+                <p>Likes: {content.likes}</p>
+              </div>
+            ))}
+          </div>
         </div>
-    )
+      );
 }
 
-export default StudyContainer
+function StudyPageBody() {
+  return (
+    <div class="study-page-body">
+      <Search />
+      <Toggle />
+    </div>
+  );
+}
+
+function StudyContainer() {
+  return (
+    <div>
+      <StudyPageHead />
+      <StudyPageBody />
+    </div>
+  );
+}
+
+export default StudyContainer;
