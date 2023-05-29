f287fb4a2a5c7305139ab4cdcddc7f628da7ef51
Yodayeong <duekdud01@naver.com>
2023-05-29 13:02:23 +0900

    feat: 스터디 크리에이트 api 통신 구현 [여다영]

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -52,33 +52,115 @@ function CreatePageHead() {
 // AIzaSyD2ktRW2Fofr45rCu_ENCXMNcDKrY9opxY
 
 function CreatePageBody() {
+  const [url, setUrl] = useState("");
+  const [title, setTitle] = useState("");
+  const [writer, setWriter] = useState("");
+  const [detail, setDetail] = useState("");
+  const [hashtag1, setHashtag1] = useState(false);
+  const [hashtag2, setHashtag2] = useState(false);
+  const [hashtag3, setHashtag3] = useState(false);
+  const [hashtag4, setHashtag4] = useState(false);
+  const [hashtag5, setHashtag5] = useState(false);
+  const [hashtag6, setHashtag6] = useState(false);
+  const [hashtag7, setHashtag7] = useState(false);
+
+  const handleClick1 = () => {
+    setHashtag1(!hashtag1);
+  };
+  const handleClick2 = () => {
+    setHashtag2(!hashtag2);
+  };
+  const handleClick3 = () => {
+    setHashtag3(!hashtag3);
+  };
+  const handleClick4 = () => {
+    setHashtag4(!hashtag4);
+  };
+  const handleClick5 = () => {
+    setHashtag5(!hashtag5);
+  };
+  const handleClick6 = () => {
+    setHashtag6(!hashtag6);
+  };
+  const handleClick7 = () => {
+    setHashtag7(!hashtag7);
+  };
+
+  const movePage = useNavigate();
+
+  async function create() {
+    var array = new Array();
+    if(hashtag1) {
+      array.push(1);
+    }
+    if(hashtag2) {
+      array.push(2);
+    }
+    if(hashtag3) {
+      array.push(3);
+    }
+    if(hashtag4) {
+      array.push(4);
+    }
+    if(hashtag5) {
+      array.push(5);
+    }
+    if(hashtag6) {
+      array.push(6);
+    }
+    if(hashtag7) {
+      array.push(7);
+    }
+
+    console.log(url, title, writer, detail, array);
+    // api 통신
+    // let item = {url, title, writer, detail, array};
+    // let result = await fetch("http://13.125.105.227:8080/member/login", {
+    //     method: 'POST',
+    //     body:JSON.stringify(item),
+    //     headers:{
+    //         "Content-Type":"application/json",
+    //         "Accept":"application/json"
+    //     },
+    // }).then(res=>{
+    //     console.log(res);
+    // })
+    // result = await result.json();
+    // localStorage.setItem("user-info", JSON.stringify(result))
+    movePage('/');
+  }
+
   return (
     <div className="study-page-body flex-col">
       <div>
         <p className="create-title">유튜브 주소</p>
-        <input className="create-input1" type="text"></input>
+        <input className="create-input1" type="text" onChange={(e)=>setUrl(e.target.value)}></input>
       </div>
       <div>
         <p className="create-title">강의 제목</p>
-        <input className="create-input1" type="text"></input>
+        <input className="create-input1" type="text" onChange={(e)=>setTitle(e.target.value)}></input>
+      </div>
+      <div>
+        <p className="create-title">작성자</p>
+        <input className="create-input1" type="text" onChange={(e)=>setWriter(e.target.value)}></input>
       </div>
       <div>
         <p className="create-title">상세 설명</p>
-        <textarea className="create-input2"></textarea>
+        <textarea className="create-input2" onChange={(e)=>setDetail(e.target.value)}></textarea>
       </div>
       <div>
         <p className="create-title">해시태그 설정</p>
         <div className="tags-box">
-            <div className="create-tag">#투자초보</div>
-            <div className="create-tag">#포트폴리오</div>
-            <div className="create-tag">#5000만원</div>
-            <div className="create-tag">#ETF</div>
-            <div className="create-tag">#저축</div>
-            <div className="create-tag">#주식유망종목</div>
-            <div className="create-tag">#주식투자방법</div>
+            <div className={`create-tag ${hashtag1 ? 'clicked' : ''}`} onClick={handleClick1}>#투자초보</div>
+            <div className={`create-tag ${hashtag2 ? 'clicked' : ''}`} onClick={handleClick2}>#포트폴리오</div>
+            <div className={`create-tag ${hashtag3 ? 'clicked' : ''}`} onClick={handleClick3}>#5000만원</div>
+            <div className={`create-tag ${hashtag4 ? 'clicked' : ''}`} onClick={handleClick4}>#ETF</div>
+            <div className={`create-tag ${hashtag5 ? 'clicked' : ''}`} onClick={handleClick5}>#저축</div>
+            <div className={`create-tag ${hashtag6 ? 'clicked' : ''}`} onClick={handleClick6}>#주식유망종목</div>
+            <div className={`create-tag ${hashtag7 ? 'clicked' : ''}`} onClick={handleClick7}>#주식투자방법</div>
         </div>
       </div>
-      <div className="create-submit">영상 업로드 하기</div>
+      <div className="create-submit" onClick={create}>영상 업로드 하기</div>
     </div>
   );
 }
